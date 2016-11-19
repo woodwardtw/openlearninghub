c609d844eb864079e811c343ec5c1f7bf35b526b
CogDog <cogdogblog@gmail.com>
2016-11-18 20:52:48 -0700

    Magic Box updated

--- page-magic-box.php
+++ page-magic-box.php
@@ -5,25 +5,24 @@ Template Name: Magic Box Form
 Provides functionality for the RSS Feed Finder
 */
 
+ 
 // no feedback for now.
-$feedback_msg = '';
+$feedback_msg = 'The Magic Box is a tool that can find the RSS feed for your blog. It works best if all content on your blog should be syndicated to this site (it will not work for site where you are syndicating by tag or category).';
 
 // verify that a form was submitted and it passes the nonce check
-if ( isset( $_POST['tv_form_add_feedfinder_submitted'] ) && wp_verify_nonce( $_POST['tv_form_add_feedfinder_submitted'], 'tv_form_add_feedfinder' ) ) {
+if ( isset( $_POST['magic_box_form_add_feedfinder_submitted'] ) && wp_verify_nonce( $_POST['magic_box_form_add_feedfinder_submitted'], 'magic_box_form_add_feedfinder' ) ) {
  
 	// grab the url from the form
-	
 	$blogurl = 	esc_url( trim($_POST['blogurl']), array('http', 'https') ); 
- 	// $blogurl =  trim( $_POST['blogurl'] );
  	
  	// let's do some validation, store an error message for each problem found
  	$errors = array();
  		
  	if ( empty( $blogurl ) ) {
  		// missing value for url
- 		$errors[] = '<strong>Blog URL Missing or Invalid</strong> - please enter the full URL for your blog.';
+ 		$errors[] = '<strong>Blog web address missing or invalid</strong> - please enter the full web address for your blog. Please test it in a browser to make sure it works, ok?';
  	} elseif ( strpos( $blogurl, '/feed' )  ) {
- 		$errors[] = '<strong>Is That Your Blog URL?</strong> It looks like you entered the address of an RSS Feed in the box. Please enter the web address where you content shows up, the URL that shows your site in a browser.';
+ 		$errors[] = '<strong>Is that really your blog\'s web address?</strong> It looks like you entered the URL for an RSS Feed in the box. Please enter the web address where you blog shows up, the URL that shows your site in a browser.';
  	 	
  	} else { 
 
@@ -31,12 +30,12 @@ if ( isset( $_POST['tv_form_add_feedfinder_submitted'] ) && wp_verify_nonce( $_P
 		$blogrss = feedSearch( $blogurl );
  	
  		// feed not found by the function
- 		if ($blogrss == -1) $errors[] = '<strong>Blog Feed cound not be found</strong>. Make sure the URL that you typed is the one that dispalys your web site in a browser. Please check, edit, and try again, or get <a href="http://thoughtvectors.net/help/">assistance</a>';
+ 		if ($blogrss == -1) $errors[] = '<strong>An RSS feed cound not be found</strong>. Make sure the web address that you typed is the one that displays your web site in a browser. Or maybe it is a web site we cannot work magic on. Please check, edit, and try again, or seek assistance.';
  	}
  	
  	if ( count($errors) > 0 ) {
  			// form errors, build feedback string to display the errors
- 			$feedback_msg = 'Sorry but the Magic Box cannot process your request: <ul>';
+ 			$feedback_msg = 'Bad Magic! Sorry but the Magic Box cannot process your request: <ul>';
  			
  			// Hah, each one is an oops, get it? 
  			foreach ($errors as $oops) {
@@ -45,13 +44,15 @@ if ( isset( $_POST['tv_form_add_feedfinder_submitted'] ) && wp_verify_nonce( $_P
  			
  			$feedback_msg .= '</ul>';
  			
+ 			
 	} else {
 	
-		$feedback_msg = '<div id="rsslink">&nbsp;<a href="' . $blogrss . '" target="_blank">' . $blogrss . '</a></div> <p><br />You should first <a href="' . $blogrss . '">verify the URL</a> (opens in a new window) -- it will show you something like may look like gibberish, but it represents the raw  source of information from your blog. It should show you something like <a href="http://www.scottgu.com/blogposts/rssreader/step11.jpg" target="_blank">this</a> or maybe <a href="http://misc.dailysplice.com/images/blog/screenshot_cbc_firefox_RSSpage.png"  target="_blank">this</a> (depending on your browser). <br /><br />
-Make sure you copy this URL to a place where you can find it again. Proceed now to the next step in the journey.<br /><br /><a class="button" style="font-size:1.3em" href="/syndicate-here/">ONWARD! CONNECT ME TO THOUGHTVECTORS.NET</a></p>';  
+		$feedback_msg = '<div id="rsslink">&nbsp;<a href="' . $blogrss . '" target="_blank">' . $blogrss . '</a></div> <p><br />You should first <a href="' . $blogrss . '">verify the URL</a> (opens in a new window) -- it will show you something like may look like gibberish, but it represents the raw source of information from your blog. It should show you something like <a href="http://www.scottgu.com/blogposts/rssreader/step11.jpg" target="_blank">this</a> (depending on your browser). <br /><br />
+		Make sure you copy this URL to a place where you can find it when you sign your nlog up. Proceed now to the next step in the journey.<br /><br /><a class="button" style="font-size:1.3em" href="/syndicate-here/">ONWARD! CONNECT MY BLOG</a></p>'; 
 	
 	}
 }
+
  		
 ?>
 <?php get_header(); ?>
@@ -89,7 +90,7 @@ Make sure you copy this URL to a place where you can find it again. Proceed now 
 	                	<input type="text" id="blogurl" name="blogurl" style="width:400px" value="<?php echo $blogurl?>" />
 	                	
 	                	
-	                	<?php wp_nonce_field( 'tv_form_add_feedfinder', 'tv_form_add_feedfinder_submitted' ); ?>
+	                	<?php wp_nonce_field( 'magic_box_form_add_feedfinder', 'magic_box_form_add_feedfinder_submitted' ); ?>
 	                	
 	                	<input type="submit" class="btn btn-primary" value="Find My Blog's RSS Feed"  id="submitblogurl" name="submitblogurl" tabindex="15">
 	                	
